4348e7639c9bd99dac2bac139af5d2d2ec3655c8
Femi Fagbure <femifagbure@gmail.com>
2020-01-02 19:13:14 -0500

    added questions to javascript

---
+++ script.js
@@ -0,0 +1,55 @@
+var begin 
+var questions = [
+    {
+        title: "What is the most basic language used to create a web page?"
+        choices: ["html", "css", "javascript", "jquery"]
+        answer: "html"
+    },
+    {
+        title: "In order to add style to a page, one must use ..."
+        choices: ["html", "booleans", "css", "c++"]
+        answer: "css"
+    }, 
+    {
+        title: "An example of a css framework is ..."
+        choices: ["strings", "jquery", "bootstrap", "bootcampspot"]
+        answer: "bootstrap"
+    },
+    {
+        title: "Javascript is used to add character to a page and make it do more fun things"
+        choices: ["true", "false"]
+        answer: "true"
+    },
+    {
+        title: "An example of a javascript framework is ..."
+        choices: ["strings", "jquery", "bootstrap", "bootcampspot"]
+        answer: "jquery"
+    },
+    {
+        title: "In order to change an object on a page without changing the html, we use ..."
+        choices: ["DOM", "True/False Statements", "css", "code"]
+        answer: "DOM"
+    },
+    {
+        title: "A website will NOT work without Javascript"
+        choices: ["true", "false"]
+        answer: "false"
+    },
+    {
+        title: "A website with ONLY HTML is flat and boring"
+        choices: ["true", "false"]
+        answer: "true"
+    },
+    {
+        title: "Where in your html do you put links to external css or javascript files?"
+        choices: ["body", "shoulder", "footer", "head"]
+        answer: "head"
+    },
+    {
+        title: "(Trick Question) Coding is FUN!"
+        choices: ["true", "false"]
+        answer: "false"
+    }
+];
+var highscores
+ 
