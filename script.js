b6a1a0ce9b7603fdf8b8ea5f947c2e76b6c45eec
Femi Fagbure <femifagbure@gmail.com>
2020-01-03 21:31:07 -0500

    got the first question sorted

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-var begin 
 var questions = [
     {
         title: "What is the most basic language used to create a web page?",
@@ -9,7 +8,7 @@ var questions = [
         title: "In order to add style to a page, one must use ...",
         choices: ["html", "booleans", "css", "c++"],
         answer: "css"
-    }, 
+    },
     {
         title: "An example of a css framework is ...",
         choices: ["strings", "jquery", "bootstrap", "bootcampspot"],
@@ -51,5 +50,71 @@ var questions = [
         answer: "false"
     }
 ];
+
+
+
+
+$(document).ready(function () {
+
+    var sec = 600;
+    let currentQuestion = 0;
+    var time;
+
+    function myTimer() {
+        $("#timer-display").text(sec + " seconds left");
+        sec--;
+        if (sec === -1) {
+            clearInterval(time);
+            alert("Time's up! You lose")
+        }
+
+    }
+
+    var div_questions = $("<div>", "list-group");
+
+
+    $(".btn-primary").click(function () {
+
+
+        // Hide opening text
+        $(".card-body").hide();
+
+        var P = $("<p>")
+        var pClass = P.addClass("list-group-item list-group-item-success")
+        var pClassText = pClass.text(questions[currentQuestion].title);
+        div_questions.append(pClassText)
+        // Timer 
+        time = setInterval(myTimer, 1000)
+
+
+        for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
+
+            var B = $("<button>")
+            var bClass = B.addClass("list-group-item list-group-item-action")
+            var bClassText = bClass.text(questions[currentQuestion].choices[i]);
+            $(B).attr('data-answer', questions[currentQuestion].choices[i]);
+            div_questions.append(bClassText)
+
+            $(B).click(function () {
+                var userAnswer = $(this).attr("data-answer");
+                if (userAnswer === questions[currentQuestion].answer) {
+                    alert("Correct!")
+
+                    // // move to next question 
+                    currentQuestion++;
+
+                } else {
+                    alert("Incorrect")
+                    // move to next question 
+                    sec -= 15;
+                }
+            });
+        }
+        $("#card-questions").append(div_questions);
+
+    });
+});
+
+
 var highscores
- 
+// store high scores to local storage
